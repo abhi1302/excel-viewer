6ab1465f6aaeaf745b4b08e053d2cef74904e0f9
Abhijeet Mandal <abhijeet.mandal@subex.com>
2025-06-09 10:47:06 +0530

    Fix port binding and switch to gunicorn for Render

--- app.py
+++ app.py
@@ -36,9 +36,8 @@ def index():
 def download():
     return send_file(processed_excel, download_name="processed_ratecard.xlsx", as_attachment=True)
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
     
